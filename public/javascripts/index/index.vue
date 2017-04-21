69e21270b22c9518329515a9b1b426e0b3eb4b0d
baozi <791736879@qq.com>
2017-04-21 21:25:18 +0800

    tijiao

--- public/javascripts/index/index.vue
+++ public/javascripts/index/index.vue
@@ -1,35 +1,43 @@
 <template>
   <div>
-    <el-menu theme="light" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="3">
-        <a href="https://www.ele.me" target="_blank">订单管理</a>
-      </el-menu-item>
-    </el-menu>
+    <el-row :gutter="0">
+      <el-col :span="5" class="topItem">
+        <p>小包总</p>
+      </el-col>
+      <el-col :span="8">
+        <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">文章</el-menu-item>
+          <el-menu-item index="3">DEMO</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="7" class="topItem">
+        <el-input placeholder="搜索" icon="search" v-model="search" :on-icon-click="handleIconClick"></el-input>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script type="text/javascript">
   export default {
     data() {
       return {
-        activeIndex: '1'
+        activeIndex: '1',
+        search: ''
       };
     },
     methods: {
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
+      },
+      handleIconClick(){
+
       }
     }
   };
 </script>
-<style lang="less">
-  a {
-    text-decoration: none;
-  }
+<style>
+   .topItem {
+      height: 60px;
+      line-height: 60px;
+   }
 </style>
